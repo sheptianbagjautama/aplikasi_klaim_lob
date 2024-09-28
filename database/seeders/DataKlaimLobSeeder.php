f751828ea0cc391752dc627db35bf7526d71f295
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2024-09-28 22:29:28 +0700

    load script SQL into DataKlaimLobSeeder

--- database/seeders/DataKlaimLobSeeder.php
+++ database/seeders/DataKlaimLobSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DataKlaimLobSeeder extends Seeder
 {
@@ -14,6 +15,8 @@ class DataKlaimLobSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $path = base_path() . '/database/seeders/data_klaim_lob.sql';
+        $sql = file_get_contents($path);
+        DB::unprepared($sql);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Wilayah_Kerja;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([
+            Wilayah_Kerja::class,
+            DataKlaimLobSeeder::class,
+            // Add other seeders here
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
